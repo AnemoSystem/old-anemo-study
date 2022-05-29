91298ca17a72696ca2623b3c0c4f5027b74d8fc5
Guilherme Samuel <guisamuel800@gmail.com>
2022-05-29 16:45:29 -0300

    fix database

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2022 at 09:28 PM
+-- Generation Time: May 29, 2022 at 09:44 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
